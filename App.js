405af50dd8b72e92715be0c925982989e8764e53
Robertoromg <robertoro_mg@outlook.com>
2020-06-26 21:52:31 -0300

    first commit

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00a6c0',
     alignItems: 'center',
     justifyContent: 'center',
   },
